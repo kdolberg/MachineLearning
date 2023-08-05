30057b93093c7dea86cb153bc03fd559f6b11da3
Kris Dolberg <kristofor.d@gmail.com>
2023-08-05 14:11:01 -0700

    added "layers.cpp"

---
+++ src/layer.cpp
@@ -0,0 +1,8 @@
+#include "types.h"
+
+LinearAlgebra::Matrix MachineLearning::calc_derivatives_to_pass_on(const LinearAlgebra::Matrix& derivatives,const LinearAlgebra::Matrix& weights) {;
+	return derivatives;
+}
+LinearAlgebra::Matrix MachineLearning::calc_partial_derivatives(const LinearAlgebra::Matrix& derivatives,const LinearAlgebra::Matrix& pre_activation_function_output) {
+	return derivatives;
+}
