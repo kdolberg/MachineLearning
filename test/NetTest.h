9cc6b9d9fd22007d021a0c197fa895cda188666f
Kris Dolberg <kristofor.d@gmail.com>
2023-09-21 11:56:42 -0700

    Moving Net tests to theNetTest module

--- test/NetTest.h
+++ test/NetTest.h
@@ -1,4 +1,29 @@
+#include "UnitTest.h"
 #include "net.h"
 
-class NetTest {	
+class NetTest {
+	typedef struct NetUintIter {
+		std::vector<MachineLearning::uint>::const_iterator def;
+		MachineLearning::Net::const_iterator n;
+		NetUintIter& operator++() {
+			++this->def;
+			++this->n;
+			return (*this);
+		}
+		bool operator==(const NetUintIter& iter) {
+			if((iter.n)==(this->n)) {
+				assert((std::next(iter.def)==this->def) || (iter.def==std::next(this->def)));
+				return true;
+			} else {
+				return false;
+			}
+		}
+	} NetUintIter;
+
+	static bool Net_constructor_gives_correct_num_inputs();
+
+	static void forward_propagation_output();
+
+public:
+	static void execute_all_tests();
 };
