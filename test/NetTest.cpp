79bdc60749b9ba36c3e12517ecaccb43a1fdd6cc
krisdolberg <kristofor.d@gmail.com>
2023-09-26 14:28:25 -0700

    Working on calculate_gradient()

--- test/NetTest.cpp
+++ test/NetTest.cpp
@@ -3,6 +3,20 @@
 #define H 0.05f
 
 void NetTest::PrivateAPI::calculate_gradient() {
+	MachineLearning::NetDef def = {2,1};
+	MachineLearning::Net n(def,1.0f); // single-node net with all parameters equal to 1
+	LinearAlgebra::Matrix x(MINDEX(2,100)),y(MINDEX(1,100));
+	x.set_contents(1.0f);
+	y.set_contents(2.0f);
+	MachineLearning::TrainingDataset td = {x,y};
+	n.load_training_data(td);
+	PRINT_VAR(n());
+	PRINT_VAR(n.calculate_gradient());
+
+	/**
+	 * TODO:	Add a bunch of different tests with neural nets of different sizes.
+	 * 			Calculate the expected results by hand
+	 */
 }
 
 void NetTest::PublicAPI::load_training_data() {
@@ -13,6 +27,7 @@ void NetTest::PublicAPI::learn() {
 }
 
 void NetTest::execute_all_tests() {
+	PrivateAPI::calculate_gradient();
 	NetTest::PublicAPI::learn();
 	NetTest::PublicAPI::load_training_data();
 }
