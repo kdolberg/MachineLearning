50a19ec1f8c182d946d4357f3eed00aeec30d658
krisdolberg <kristofor.d@gmail.com>
2023-11-26 15:01:59 -0800

    Produces assert fail: "assertion "m.get_num_rows()" failed: file "src/save_load.cpp", line 15, function: void array_copy(const scalar*, LinearAlgebra::Matrix&)" SaveLoadTest: Added "PRINT_FUNC" to the tests Added "test_list" function to test multiple types of lists easily Added "list()", "ActivationFunction()", and "Net()" to execute_all_tests

--- test/SaveLoadTest.cpp
+++ test/SaveLoadTest.cpp
@@ -1,7 +1,13 @@
 #include "machine_learning.h"
 #include "SaveLoadTest.h"
 
+static int get_file_num() {
+	static int i = 0;
+	return i++;
+}
+
 void SaveLoadTest::LayerParams() {
+	PRINT_FUNC();
 	LinearAlgebra::Matrix w(5,true),b(MINDEX(5,1));
 	b[MINDEX(0,0)] = 1;
 	MachineLearning::LayerParams lp1 = {w,b};
@@ -12,6 +18,7 @@ void SaveLoadTest::LayerParams() {
 }
 
 void SaveLoadTest::Matrix() {
+	PRINT_FUNC();
 	LinearAlgebra::Matrix m1(5,true), m2;
 	MachineLearning::save(m1,"test/saves/m.mat");
 	MachineLearning::load(m2,"test/saves/m.mat");
@@ -19,6 +26,7 @@ void SaveLoadTest::Matrix() {
 }
 
 void SaveLoadTest::TrainingDataset() {
+	PRINT_FUNC();
 	LinearAlgebra::Matrix x(5,true),y(MINDEX(5,1));
 	x.randomize();
 	y.randomize();
@@ -28,8 +36,52 @@ void SaveLoadTest::TrainingDataset() {
 	TEST_RETURN_FUNC(td1,==,td2);
 }
 
+void SaveLoadTest::Net() {
+	PRINT_FUNC();
+	MachineLearning::Net n1(MachineLearning::NetDef({5,4,3,2,1})), n2;
+	TEST_RETURN_FUNC(MachineLearning::save(n1,"test/saves/testnet.nn"),==,true);
+	TEST_RETURN_FUNC(MachineLearning::load(n2,"test/saves/testnet.nn"),==,true);
+	TEST_RETURN_FUNC(n1,==,n2);
+}
+
+void SaveLoadTest::ActivationFunction() {
+	PRINT_FUNC();
+	{
+		MachineLearning::ActivationFunction af1, af2;
+		af1 = MachineLearning::get_sigmoid();
+		TEST_RETURN_FUNC(MachineLearning::save(af1,"test/saves/sigmoid.af"),==,true);
+		TEST_RETURN_FUNC(MachineLearning::load(af2,"test/saves/sigmoid.af"),==,true);
+		TEST_RETURN_FUNC(af1.str(),==,SIGMOID_NAME);
+		TEST_RETURN_FUNC(af2.str(),==,SIGMOID_NAME);
+		TEST_RETURN_FUNC(af1,==,af2);
+	}{
+		MachineLearning::ActivationFunction af1, af2;
+		af1 = MachineLearning::get_leaky_ReLU();
+		TEST_RETURN_FUNC(MachineLearning::save(af1,"test/saves/leakyrelu.af"),==,true);
+		TEST_RETURN_FUNC(MachineLearning::load(af2,"test/saves/leakyrelu.af"),==,true);
+		TEST_RETURN_FUNC(af1.str(),==,LEAKY_RELU_NAME);
+		TEST_RETURN_FUNC(af2.str(),==,LEAKY_RELU_NAME);
+		TEST_RETURN_FUNC(af1,==,af2);
+	}
+}
+
+#define TEST_LIST(__type__,__var__) std::cout << #__type__ << std::endl;\
+									SaveLoadTest::test_list<__type__>((std::list<__type__>) __var__);
+
+void SaveLoadTest::list() {
+	PRINT_FUNC();
+	MachineLearning::Net n(MachineLearning::NetDef({5,4,3,2,1}));
+	TEST_LIST(MachineLearning::LayerParams,n);
+	TEST_LIST(MachineLearning::ActivationFunction,n.get_activation_function_list());
+	// SaveLoadTest::test_list<MachineLearning::LayerParams>((std::list<MachineLearning::LayerParams>)n);
+	// SaveLoadTest::test_list<MachineLearning::ActivationFunction>(n.get_activation_function_list());
+}
+
 void SaveLoadTest::execute_all_tests() {
+	SaveLoadTest::list();
 	SaveLoadTest::LayerParams();
 	SaveLoadTest::Matrix();
 	SaveLoadTest::TrainingDataset();
+	SaveLoadTest::ActivationFunction();
+	SaveLoadTest::Net();
 }
--- test/SaveLoadTest.h
+++ test/SaveLoadTest.h
@@ -1,8 +1,29 @@
 #include "UnitTest.h"
 
+#define PRINT_FUNC() std::cout << "\nEXECUTING TESTS FOR " << __FUNCTION__ << "\n";
+
+static int get_file_num();
+
 namespace SaveLoadTest {
+	template <typename T>
+	void test_list(std::list<T> l1) {
+		std::string str = std::string("test/saves/list") + std::to_string(get_file_num()) + std::string(".list");
+		std::list<T> l2;
+		TEST_RETURN_FUNC(MachineLearning::save(l1,str.c_str()),==,true);
+		TEST_RETURN_FUNC(MachineLearning::load(l2,str.c_str()),==,true);
+		TEST_RETURN_FUNC(l1,==,l2);
+	}
+
+	// Test functions
+	void list();
 	void Matrix();
 	void LayerParams();
 	void TrainingDataset();
+	void ActivationFunction();
+	void Net();
+
+	/**
+	 * @brief Executes all tests
+	 */
 	void execute_all_tests();
 } // SaveLoadTest
