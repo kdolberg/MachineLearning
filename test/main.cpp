f11dada1f9374649b17806fbd4ac3c0722dbb02d
krisdolberg <kristofor.d@gmail.com>
2023-11-26 14:43:57 -0800

    Added "compilation date" to the beginning of main

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 
 int main(int argc, char const *argv[]) {
+	std::cout << "Compilation date: " << __DATE__ << " " << __TIME__ << std::endl;
 	NetTest::execute_all_tests();
 	ActFuncTest::execute_all_tests();
 	SaveLoadTest::execute_all_tests();
